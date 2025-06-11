f7e3aebc5d523186e40bcae94d16933837c43fbc
nebula682 <malcolmnie@gmail.com>
2025-06-11 10:37:46 +0200

    changes

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,108 +1,109 @@
+
+
+
+
+
+
+
+
+
+
+
 const mongodb = require('../data/database');
-const ObjectId  = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-                // GET /courses
-// #swagger.tags = ['Courses']
-// #swagger.description = 'Gets all courses'
-                const result = await mongodb.getDatabase().db().collection('courses').find();
-                result.toArray().then((courses) => {
-                                res.setHeader('Content-Type', 'application/json');
-                                res.status(200).json(courses);
-
-});
+  try {
+    const result = await mongodb.getDatabase().db().collection('courses').find();
+    const courses = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(courses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-                             // GET /courses
-// #swagger.tags = ['Courses']
-// #swagger.description = 'Gets single'   
-                const courseId = new ObjectId(req.params.id);
-                const result = await mongodb.getDatabase().db().collection('courses').find({ _id: courseId});
-                result.toArray().then((courses) => {
-                                res.setHeader('Content-Type', 'application/json');
-                                res.status(200).json(courses[0]);
-
-                });
-
-};
-const createCourse = async (req, res) =>{
-                //#swagger.tags=['courses']
-                
-                const course = {
-                                courseName:req.body.courseNameName,
-                                department:req.body.department,
-                                credits:req.body.description,
-                                instructor:req.body.instructor,
-                                startDate:req.body.startDate,
-                                endDate:req.body.endDate,
-                                schedule:req.body.schedule,
-                                prerequisites:req.body.prerequisites,
-                                level:req.body.level
-
-};
-const response = await mongodb.getDatabase().db().collection('courses').insertOne(course) ;
-if (response.acknowledged){
-                res.status(204).json({ message: 'Course created successfully' }); 
-
-} else{
-                res.status(500).json(response.error ||' Some error occurred while updating the Course');
-}
-};
-const updateCourse = async (req, res) =>{
-                //#swagger.tags=['courses']
-                const courseId = new ObjectId(req.params.id);
-                const course = {
-                                  courseName:req.body.courseNameName,
-                                department:req.body.department,
-                                credits:req.body.description,
-                                instructor:req.body.instructor,
-                                startDate:req.body.startDate,
-                                endDate:req.body.endDate,
-                                schedule:req.body.schedule,
-                                prerequisites:req.body.prerequisites,
-                                level:req.body.level
-
-};
-                                
-
-const response = await mongodb.getDatabase().db().collection('courses').replaceOne({ _id: courseId }, course);
-if (response.modifiedCount > 0) {
-                res.status(204).send();
-                } else {
-                                res.status(500).json(response.error || 'Some error occured while updating the course.');
-}
+  try {
+    const courseId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('courses').find({ _id: courseId });
+    const courses = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(courses[0]);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
+const createCourse = async (req, res) => {
+  try {
+    const course = {
+      courseName: req.body.courseName,
+      department: req.body.department,
+      credits: req.body.credits,
+      instructor: req.body.instructor,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      schedule: req.body.schedule,
+      prerequisites: req.body.prerequisites,
+      level: req.body.level
+    };
 
+    const response = await mongodb.getDatabase().db().collection('courses').insertOne(course);
+    if (response.acknowledged) {
+      res.status(201).json({ message: 'Course created successfully' });
+    } else {
+      res.status(500).json({ message: 'Some error occurred while creating the course' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
+const updateCourse = async (req, res) => {
+  try {
+    const courseId = new ObjectId(req.params.id);
+    const course = {
+      courseName: req.body.courseName,
+      department: req.body.department,
+      credits: req.body.credits,
+      instructor: req.body.instructor,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      schedule: req.body.schedule,
+      prerequisites: req.body.prerequisites,
+      level: req.body.level
+    };
 
-
-
-
-
-
-
-
+    const response = await mongodb.getDatabase().db().collection('courses').replaceOne({ _id: courseId }, course);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ message: 'Some error occurred while updating the course' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const deleteCourse = async (req, res) => {
-                const courseId = new ObjectId(req.params.id);
-                const response = await mongodb.getDatabase().db().collection('courses').deleteOne({ _id: courseId},);
-                if (response.deletedCount > 0) {
-                                res.status(204).send();
-                                } else {
-                                                res.status(500).json(response.error || 'Some error occurred while deleting the Course');
-                }
-
-}
-
-
-module.exports ={
-                getAll,
-                getSingle,
-                createCourse,
-                updateCourse,
-                deleteCourse
-
-
+  try {
+    const courseId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('courses').deleteOne({ _id: courseId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ message: 'Some error occurred while deleting the course' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
+
+module.exports = {
+  getAll,
+  getSingle,
+  createCourse,
+  updateCourse,
+  deleteCourse
+};n
--- controllers/students.js
+++ controllers/students.js
@@ -1,102 +1,102 @@
+
+
+
+
+
+
+
+
 const mongodb = require('../data/database');
-const ObjectId  = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-                 // GET /students
-// #swagger.tags = ['students']
-// #swagger.description = 'Gets all students'
-
-                const result = await mongodb.getDatabase().db().collection('students').find();
-                result.toArray().then((students) => {
-                                res.setHeader('Content-Type', 'application/json');
-                                res.status(200).json(students);
-
-});
+  try {
+    const result = await mongodb.getDatabase().db().collection('students').find();
+    const students = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(students);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res) => {
-                //#swagger.tags=['students']
-                const studentId = new ObjectId(req.params.id);
-                const result = await mongodb.getDatabase().db().collection('students').find({ _id: studentId});
-                result.toArray().then((students) => {
-                                res.setHeader('Content-Type', 'application/json');
-                                res.status(200).json(students[0]);
-
-                });
-
-};
-const createStudent = async (req, res) =>{
-                //#swagger.tags=['students']
-                
-                const student = {
-                                firstName:req.body.firstName,
-                                lastName:req.body.lastName,
-                                email:req.body.email,
-                                enrollmentDate:req.body.enrollmentDate,
-                                courses:req.body.courses,
-                                gpa:req.body.gpa,
-                                dateOfBirth:req.body,dateOfBirth
-};
-const response = await mongodb.getDatabase().db().collection('students').insertOne(student) ;
-if (response.acknowledged){
-                res.status(204).json({ message: 'Student created successfully' }); 
-
-} else{
-                res.status(500).json(response.error ||' Some error occurred while updating the Student');
-}
-};
-const updateStudent = async (req, res) =>{
-                //#swagger.tags=['students']
-                const studentId = new ObjectId(req.params.id);
-                const student = { 
-                                firstName:req.body.firstName,
-                                lastName:req.body.lastName,
-                                email:req.body.email,
-                                enrollmentDate:req.body.enrollmentDate,
-                                courses:req.body.courses,
-                                gpa:req.body.gpa,
-                                dateOfBirth:req.body,dateOfBirth
-                                 
-};
-                                
-
-const response = await mongodb.getDatabase().db().collection('students').replaceOne({ _id: studentId }, student);
-if (response.modifiedCount > 0) {
-                res.status(204).send();
-                } else {
-                                res.status(500).json(response.error || 'Some error occured while updating the student.');
-}
+  try {
+    const studentId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('students').find({ _id: studentId });
+    const students = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(students[0]);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
+const createStudent = async (req, res) => {
+  try {
+    const student = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      enrollmentDate: req.body.enrollmentDate,
+      courses: req.body.courses,
+      gpa: req.body.gpa,
+      dateOfBirth: req.body.dateOfBirth
+    };
 
+    const response = await mongodb.getDatabase().db().collection('students').insertOne(student);
+    if (response.acknowledged) {
+      res.status(201).json({ message: 'Student created successfully' });
+    } else {
+      res.status(500).json({ message: 'Some error occurred while creating the student' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
+const updateStudent = async (req, res) => {
+  try {
+    const studentId = new ObjectId(req.params.id);
+    const student = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      enrollmentDate: req.body.enrollmentDate,
+      courses: req.body.courses,
+      gpa: req.body.gpa,
+      dateOfBirth: req.body.dateOfBirth
+    };
 
-
-
-
-
-
-
-
+    const response = await mongodb.getDatabase().db().collection('students').replaceOne({ _id: studentId }, student);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ message: 'Some error occurred while updating the student' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const deleteStudent = async (req, res) => {
-                const contactId = new ObjectId(req.params.id);
-                const response = await mongodb.getDatabase().db().collection('students').deleteOne({ _id: studentId},);
-                if (response.deletedCount > 0) {
-                                res.status(204).send();
-                                } else {
-                                                res.status(500).json(response.error || 'Some error occurred while deleting the Student');
-                }
+  try {
+    const studentId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('students').deleteOne({ _id: studentId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ message: 'Some error occurred while deleting the student' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-}
-
-
-module.exports ={
-                getAll,
-                getSingle,
-                createStudent,
-                updateStudent,
-                deleteStudent
-
-
+module.exports = {
+  getAll,
+  getSingle,
+  createStudent,
+  updateStudent,
+  deleteStudent
 };
